01ddcfe11286ed36d01486b0979ca64ec4e6db51
VatsalPatel14 <152676820+VatsalPatel14@users.noreply.github.com>
2024-01-04 19:08:10 +0530

    LUassignment.py
    Day 2 Assignment
    Write a Python program to classify a given number into one of three categories: negative, zero, or positive. The program should take an integer input and print out one of the following statements based on the input:
    
    
    "The number is negative." if the number is less than zero.
    "The number is zero." if the number is exactly zero.
    "The number is positive." if the number is more than zero.

--- assignment.py
+++ assignment.py
@@ -1,1 +1,9 @@
-print(hello)
+print("enter a number")
+print(input(number:))
+if(number>0):
+  print("the number is positive.")
+elif(number<0):
+  print("the number is negative")
+else:
+  print("the number is zero")
+print("thank you")
